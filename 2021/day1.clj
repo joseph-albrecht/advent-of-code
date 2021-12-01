55cd70ee23a05347c24004f0282eba2c6b172cd1
Joey Albrecht <joey@joseph-albrecht.me>
2021-12-01 16:48:28 -0500

    add original solutions to 2021 day 1 in clojure

--- 2021/day1.clj
+++ 2021/day1.clj
@@ -3,7 +3,18 @@
   (:require [clojure.string :as string]
             [clojure.java.io :as io]))
 
-;; part one
+;; original part one
+(comment
+  (let [windows (-> (slurp "inputs/day1.txt")
+                    (string/split #"\n")
+                    (->> (map #(Integer/parseInt %))))]
+    (reduce (fn [[counter lst] el]
+              (if (> el lst)
+                [(inc counter) el]
+                [counter el]))
+            [0 (first windows)])))
+
+;; improved part one
 (-> (slurp "inputs/day1.txt")
     (string/split #"\n")
     (->> (map #(Integer/parseInt %))
@@ -11,7 +22,20 @@
          (filter (fn [[one two]] (< one two)))
          count));; => 1791
 
-;; part two
+;; original part two
+(comment
+  (let [windows (-> (slurp "inputs/day1.txt")
+                    (string/split #"\n")
+                    (->> (map #(Integer/parseInt %))
+                         (partition 3 1)))]
+    (reduce (fn [[counter lst] el]
+              (if (> (reduce + el) (reduce + lst))
+                [(inc counter) el]
+                [counter el]))
+            [0 (first windows)]
+            windows)))
+
+;; improved part two
 (-> (slurp "inputs/day1.txt")
     (string/split #"\n")
     (->> (map #(Integer/parseInt %))
